410e4e7aa7878b6db5aa3247c17522a13ec4cda9
KeoBunHai <156315580+KeoBunHai@users.noreply.github.com>
2024-02-15 19:54:52 +0700

    Add files via upload

---
+++ customer.java
@@ -0,0 +1,29 @@
+package javasecond;
+
+public class Customer {
+    private String customerName;
+    private String customerType;
+
+    public Customer() {}
+
+    public Customer(String customerName, String customerType) {
+        this.customerName = customerName;
+        this.customerType = customerType;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+}
---
+++ discountrate.java
@@ -0,0 +1,16 @@
+package javasecond;
+
+public class DiscountRate {
+
+        public double getServiceMemberDiscount() {
+                if(setcustomer_Type=="Premium"){
+                        return 0.2;
+                }else if(setcustomer_Type="Gold"){
+                        return 0.15;
+                }else if()
+        }
+
+        public double getProductMemberDiscount() {
+                return 0.05; // Example discount rate (5%)
+        }
+}
